640d342cc51f0dd881fb020490f314da43201341
Matthijs Boet <m.boet@chello.nl>
2023-08-29 16:45:30 +0200

    finished hero and shoe component

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -16,7 +16,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
 			<div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
 				<img
 					src={imgURL.thumbnail}
-					alt='shoe colletion'
+					alt='shoe collection'
 					width={127}
 					height={103.34}
 					className='object-contain'
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -49,19 +49,22 @@ const Hero = () => {
 
 			<div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
 				<img
-					src={bigShoe1}
-					alt='shoe collection'
+					src={bigShoeImg}
+					alt='shoe colletion'
 					width={610}
-					height={500}
-					className='z-10 relative object-contain'
+					height={502}
+					className='object-contain relative z-10'
 				/>
 
 				<div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
-					{shoes?.map((shoe) => (
-						<div key={shoe}>
+					{shoes.map((image, index) => (
+						<div key={index}>
 							<ShoeCard
-								imgURL={shoe}
-								changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+								index={index}
+								imgURL={image}
+								changeBigShoeImage={(shoe) =>
+									setBigShoeImg(shoe)
+								}
 								bigShoeImg={bigShoeImg}
 							/>
 						</div>
